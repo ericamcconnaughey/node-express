30183fc15a8fbfec0a1dfd0c0ff4a3190310a5da
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-03-14 09:47:49 -0500

    add routes for campsiteId in campsiteRouter

--- server.js
+++ server.js
@@ -12,25 +12,6 @@ app.use(bodyParser.json());
 
 app.use('/campsites', campsiteRouter);
 
-// app.get('/campsites/:campsiteId', (req, res) => {
-//   res.end(`Will send details of campsite ${req.params.campsiteId} to you`);
-// });
-
-// app.post('/campsites/:campsiteId', (req, res) => {
-//   res.statusCode = 403;
-//   res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}`);
-// });
-
-// app.put('/campsites/:campsiteId', (req, res) => {
-//   res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-//   res.end(`Will update the campsite: ${req.body.name}
-//       with description: ${req.body.description}`);
-// });
-
-// app.delete('/campsites/:campsiteId', (req, res) => {
-//   res.end(`Deleting campsite: ${req.params.campsiteId}`);
-// });
-
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
